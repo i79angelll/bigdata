7d52b546a64ad6b52678102509f16b6d4543c74e
i79angelll <67677575+i79angelll@users.noreply.github.com>
2020-07-02 18:04:02 +0900

    Add files via upload

---
+++ chapter3.py
@@ -0,0 +1,77 @@
+money = True
+
+if money:
+
+    print("택시를 타고 가라")
+
+else:
+
+    print("걸어 가라")
+
+
+
+money = True
+
+if money:
+
+    print("택시를")
+
+print("타고")
+
+    print("가라")
+
+
+x = 3
+
+y = 2
+
+x > y
+
+True
+
+
+
+money = 2000
+
+card = True
+
+if money >= 30000 or card:
+
+    print("택시를 타고 가라")
+
+else:
+
+    print("걸어가라")
+
+
+"a" in ("a", "b", "c")
+
+True
+
+
+pocket = ["paper", "cellphone", "money"]
+
+if "money" in pocket:
+
+    print("택시를 타고 가라")
+
+else:
+
+    print("걸어 가라")
+
+
+pocket = ["paper", "money", "cellphone"]
+
+if "money" in pocket:
+
+    print("택시를 타고 가라")
+
+
+else:
+
+    print("걸어가라")
+
+
+message = "success" if score >= 60 else "failure"
+
+
